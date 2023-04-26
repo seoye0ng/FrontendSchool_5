a7204099e538129b4127e8dd7fb190c6751437b6
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-04-26 22:30:20 +0900

    Feat: 추가한 메모들을 보여주는 기능 구현

--- JavaScript-과제/Memo/main.js
+++ JavaScript-과제/Memo/main.js
@@ -11,6 +11,9 @@ const editor = new Editor({
   placeholder: '메모를 입력해 주세요.',
 });
 
+showMemo();
+
+
 // 메모 추가하기
 function addMemo() {
   const title = document.querySelector('.memo-title').value;
@@ -24,4 +27,34 @@ function addMemo() {
   // 인풋값 초기화 해주기
   document.querySelector('.memo-title').value = '';
   editor.setHTML() = '';
+}
+
+// 메모 보여주기
+function showMemo() {
+  const memoList = document.querySelector('.memo-list');
+  memoList.innerHTML = ''; // memoList 초기화해주기
+
+  for (const memo of memos.reverse()) {
+    const memoItem = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    const memoTitle = document.createElement('h2');
+    const memoContent = document.createElement('p');
+
+    deleteBtn.innerHTML = '<i class="fa-solid fa-minus"></i>';
+    memoTitle.textContent = memo.title;
+    memoContent.innerHTML = memo.content;
+
+    memoList.appendChild(memoItem);
+    memoItem.appendChild(deleteBtn);
+    memoItem.appendChild(memoTitle);
+    memoItem.appendChild(memoContent);
+
+    deleteBtn.addEventListener('click', (event) => {
+      if (confirm('삭제 하시겠습니까?')) {
+        memoItem.remove(event.target);
+        memos.splice(event.target, 1);
+        localStorage.setItem('memos', JSON.stringify(memos));
+      }
+    });
+  }
 }
