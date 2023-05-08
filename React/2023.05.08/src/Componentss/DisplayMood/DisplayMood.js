18be3cb4edb8c5a4256a20d79910b16e1e9d3918
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-05-09 00:05:57 +0900

    Delete React/2023.05.08/src directory

--- React/2023.05.08/src/Componentss/DisplayMood/DisplayMood.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './DisplayMood.css';
-
-export default function DisplayMood(props) {
-  return <div className="container">{props.mood ? `기분이 ${props.mood}` : `선택해주세요`}</div>;
-}
